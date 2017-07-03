cffd129eb1cef1434f38c720dd342f3b85aa7f9f
hipra <gyorgy.bokros@gmail.com>
2017-07-03 20:26:33 +0200

    w

--- konyvbenjobbvolt/scripts/data.js
+++ konyvbenjobbvolt/scripts/data.js
@@ -2,20 +2,202 @@ var myObj, movies;
 myObj = {
   "movies": [
       {
-          "title": "Harry Potter and the Sorcerer's Stone",
+          "title": "Harry Potter and the Philosopher's Stone",
           "answer": "Igen.",
-          "URLImdb": "tt0241527"
+          "URLImdb": "#",
+          "URLGoodreads": "#"
 
       },
       {
           "title": "Harry Potter and the Chamber of Secrets",
           "answer": "Igen.",
-          "URLImdb": "tt0295297"
+          "URLImdb": "#",
+          "URLGoodreads": "#"
       },
       {
           "title": "Harry Potter and the Prisoner of Azkaban",
           "answer": "Igen.",
-          "URLImdb": "tt0304141"
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Harry Potter and the Goblet of Fire",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+
+      },
+      {
+          "title": "Harry Potter and the Order of the Phoenix",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Harry Potter and the Half-Blood Prince",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Harry Potter and the Deathly Hallows",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+
+      },
+      {
+          "title": "Arrival",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "The 100 Year-Old Man Who Climbed Out the Window and Disappeared",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Help",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+
+      },
+      {
+          "title": "Stardust",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "American Gods",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Sin City",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+
+      },
+      {
+          "title": "Watchmen",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Jonathan Strange & Mr Norrell",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+
+      },
+      {
+          "title": "The Adjustment Bureau",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Minority Report",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Scanner Darkly",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "The Shining",
+          "answer": "Nem.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "IT",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Carrie",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "The Running Man",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Pet Sematary",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Misery",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Needful Things",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Dolores Claiborne",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Christine",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "American Psycho",
+          "answer": "Igen.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Tinker Tailor Soldier Spy",
+          "answer": "Nem.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Mystic River",
+          "answer": "Nem.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
+      },
+      {
+          "title": "Stand by Me",
+          "answer": "Nem.",
+          "URLImdb": "#",
+          "URLGoodreads": "#"
       }
     ]
   }
